ec88164f4e9945b47ea16b591511c28c7be9d519
infinitymeme <raferise@gmail.com>
2020-05-02 22:44:03 -0400

    adds to shortest col first. todo change scaling to width based not horizontal-landscape

--- scripts.js
+++ scripts.js
@@ -25,32 +25,59 @@ function getViewport() {
  return [viewPortWidth, viewPortHeight];
 }
 
+function getSmallestImageColumn() {
+	var jsimages = document.getElementById('jsimages');
+	var minheight = jsimages.children[0].offsetHeight;
+	var minheightindex = 0;
+	for (var i=1; i<jsimages.childElementCount; i++) {
+		if (jsimages.children[i].offsetHeight < minheight) {
+			minheight = jsimages.children[i].offsetHeight;
+			minheightindex = i;
+		}
+	}
+	return jsimages.children[minheightindex];
+
+}
+
+function createImageElement(imgnum) {
+	var img = document.createElement('img');
+	img.src = "./images/"+(imgnum++)+".jpg";
+	return img;
+}
+
+var IMAGE_COUNT = 10;
+var COLUMN_WIDTH_LANDSCAPE = 300;
+var COLUMN_WIDTH_PORTRAIT = 200;
 function onWindowResize() {
 	let [vw, vh] = getViewport();
-	var packhere = document.getElementById('jsimages');
-	packhere.innerHTML = '';
-	document.getElementById('debug').innerHTML = "WAIT";
+	var jsimages = document.getElementById('jsimages');
+	jsimages.innerHTML = '';
 	if (vw > vh) { //landscape
 		document.body.classList.add('landscape');
 		document.body.classList.remove('portrait');
 		var imgnum = 0;
-		var cols = [];
-		for (var i=0; i<vw-320; i+=300) {
+		for (var i=0; i<vw-20-COLUMN_WIDTH_LANDSCAPE; i+=COLUMN_WIDTH_LANDSCAPE) {
 			var col = document.createElement('div');
 				col.classList.add('imgcolumn');
-			packhere.appendChild(col);
-			cols.push(col);
+				col.setAttribute('style','width: '+COLUMN_WIDTH_LANDSCAPE+"px");
+			jsimages.appendChild(col);
 		}
-		let IMAGECOUNT = 10; //TODO COUNT IMAGES
-		for (var i=0; i<IMAGECOUNT; i++) {
-			var img = document.createElement('img');
-				img.src = "./images/"+(imgnum++)+".jpg";
-			cols[i%cols.length].appendChild(img);
+		for (var i=0; i<IMAGE_COUNT; i++) {
+			getSmallestImageColumn().appendChild(createImageElement(i));
 		}
-		document.getElementById('debug').innerHTML = i/300 +" "+vw;
 	} else { //portrait
 		document.body.classList.add('portrait');
 		document.body.classList.remove('landscape');
+		var imgnum = 0;
+		for (var i=0; i<vw-20-COLUMN_WIDTH_PORTRAIT; i+=COLUMN_WIDTH_PORTRAIT) {
+			var col = document.createElement('div');
+				col.classList.add('imgcolumn');
+				col.setAttribute('style','width: '+COLUMN_WIDTH_PORTRAIT+"px");
+			jsimages.appendChild(col);
+		}
+		for (var i=0; i<IMAGE_COUNT; i++) {
+			getSmallestImageColumn().appendChild(createImageElement(i));
+		}
 	}
 }
 window.addEventListener('resize', onWindowResize);
