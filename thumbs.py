50090f15c44900e6e24578071ce9a9cdb93ef94c
infinitymeme <raferise@gmail.com>
2020-05-04 02:47:52 -0400

    increase thumnail quality and autodetect rotation

--- thumbs.py
+++ thumbs.py
@@ -4,12 +4,22 @@ if not len(sys.argv) == 3:
     print("firstImageNumberToThumbnail, imagesToThumbnail")
     quit()
    
-from PIL import Image
+from PIL import Image, ExifTags
 print("generating thumbs...")
 for i in range(int(sys.argv[1]),int(sys.argv[2])):
-    image = Image.open(".//images//"+str(i)+".jpg").convert("RGB")
-    MAX_SIZE = (300, 2048)
-    image.thumbnail(MAX_SIZE) 
+    image = Image.open(".//images//"+str(i)+".jpg")
+    try:
+        exif=dict((ExifTags.TAGS[k], v) for k, v in image._getexif().items() if k in ExifTags.TAGS)
+        if exif['Orientation'] == 3 : 
+            image=image.rotate(180, expand=True)
+        elif exif['Orientation'] == 6 : 
+            image=image.rotate(270, expand=True)
+        elif exif['Orientation'] == 8 : 
+            image=image.rotate(90, expand=True)
+    except:
+        pass
+    image = image.convert("RGB")
+    image.thumbnail((300, 2048), Image.ANTIALIAS) 
     image.save('.//thumbs//'+str(i)+'.jpg', "JPEG", quality=100)
     print('/thumbs/'+str(i)+'.jpg')
 print("done")
