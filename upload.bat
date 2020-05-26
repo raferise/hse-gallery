538a071424112f2a42583fa9cd03cdfbaaf6408a
infinitymeme <raferise@gmail.com>
2020-05-26 16:45:05 -0400

    change sort order

--- upload.bat
+++ upload.bat
@@ -2,11 +2,11 @@
 
 setlocal EnableDelayedExpansion
 SET i=0
-FOR /F "tokens=*" %%A IN ('dir .\images /B') DO SET /A i =!i!+1
+FOR /F "tokens=*" %%A IN ('dir .\images /B /ON') DO SET /A i =!i!+1
 echo Found %i% images already existing. Ready to upload. NOTE: All files in /upload/ must be jp(e)g.
 pause
 SET s=%i%
-FOR /F "tokens=*" %%h IN ('dir .\upload /B') do (
+FOR /F "tokens=*" %%h IN ('dir .\upload /B /ON') do (
  move ".\upload\%%h" ".\images\!i!.jpg"
  set /a i=!i!+1
 )
